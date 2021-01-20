f5a3d97693ab10d4da77220fd3dfef30760685cc
Hicham SADDEK <36214361+hicham-saddek@users.noreply.github.com>
2021-01-20 21:40:54 +0100

    Update Authorizer.php

--- src/Concerns/Authorizer.php
+++ src/Concerns/Authorizer.php
@@ -21,7 +21,7 @@ trait Authorizer
      * @param Role|int|array $role
      * @return array
      */
-    public function addRole(int|array|Role $role)
+    public function addRole($role)
     {
         return $this->roles()->syncWithoutDetaching($role);
     }
@@ -38,7 +38,7 @@ trait Authorizer
      * @param Role|int|array $role
      * @return int
      */
-    public function removeRole(int|array|Role $role)
+    public function removeRole($role)
     {
         return $this->roles()->detach($role);
     }
@@ -47,7 +47,7 @@ trait Authorizer
      * @param Role|int|string $role
      * @return mixed
      */
-    public function hasRole(Role|int|string $role)
+    public function hasRole($role)
     {
         if ($role instanceof Model) return $this->roles()->whereKey($role->getKey())->exists();
         if (is_int($role)) return $this->roles()->whereKey($role)->exists();
@@ -58,7 +58,7 @@ trait Authorizer
      * @param int|string|Permission $perm
      * @return bool
      */
-    public function hasPermission(Permission|string|int $perm)
+    public function hasPermission($perm)
     {
         return $this->roles()->whereHas('permissions', function (Builder $permission) use ($perm) {
             if ($perm instanceof Model)
