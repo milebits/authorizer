1b065eb14823331e6352dd22c6d9ac182b14ba9a
Hicham SADDEK <h.saddek@serrahub.com>
2021-01-20 23:11:13 +0100

    Adding role user relations
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

--- config/authorizer.php
+++ config/authorizer.php
@@ -2,9 +2,6 @@
 
 return [
     'pivots' => [
-        'user_class' => 'App\Models\User',
-        'permissions' => [
-            'verbs' => ['viewAny' => "View any", 'view' => "View", 'update' => "Update", 'delete' => "Delete", 'forceDelete' => "Force delete"],
-        ],
+        'user_class' => null, // Leave null for automatic setup
     ],
 ];
