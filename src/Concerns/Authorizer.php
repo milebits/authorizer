b2b8d83c9af5bc382cec6249400a9dc346f30198
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-18 00:05:27 +0100

    PSR changes Changing lookup method for permissions, use now class, action method instead of slug.
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

--- src/Concerns/Authorizer.php
+++ src/Concerns/Authorizer.php
@@ -6,7 +6,6 @@ namespace Milebits\Authorizer\Concerns;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Milebits\Authorizer\Models\Permission;
 use Milebits\Authorizer\Models\Role;
 
 /**
@@ -21,15 +20,15 @@ trait Authorizer
      * @param Role|int|array $role
      * @return array
      */
-    public function addRole($role)
+    public function addRole(Role|array|int $role): array
     {
-        return $this->roles()->syncWithoutDetaching($role);
+        return $this->roles()->syncWithoutDetaching(is_array($role) ? $role : [$role]);
     }
 
     /**
      * @return BelongsToMany
      */
-    public function roles()
+    public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class)->withTimestamps();
     }
@@ -38,16 +37,16 @@ trait Authorizer
      * @param Role|int|array $role
      * @return int
      */
-    public function removeRole($role)
+    public function removeRole(Role|array|int $role): int
     {
-        return $this->roles()->detach($role);
+        return $this->roles()->detach(is_array($role) ? $role : [$role]);
     }
 
     /**
      * @param Role|int|string $role
-     * @return mixed
+     * @return bool
      */
-    public function hasRole($role)
+    public function hasRole(Role|int|string $role): bool
     {
         if ($role instanceof Model) return $this->roles()->whereKey($role->getKey())->exists();
         if (is_int($role)) return $this->roles()->whereKey($role)->exists();
@@ -55,17 +54,19 @@ trait Authorizer
     }
 
     /**
-     * @param int|string|Permission $perm
+     * @param string|Model $class
+     * @param string|null $action
      * @return bool
      */
-    public function hasPermission($perm)
+    public function hasPermission(string|Model $class, string $action = null): bool
     {
-        return $this->roles()->whereHas('permissions', function (Builder $permission) use ($perm) {
-            if ($perm instanceof Model)
-                return $permission->whereKey($perm->getKey());
-            if (is_int($perm))
-                return $permission->whereKey($perm);
-            return $permission->whereKey(Permission::slugLike($perm)->first());
+        return $this->roles()->whereHas('permissions', function (Builder $permission) use ($class, $action) {
+            if (is_array($class))
+                [$class, $action] = $class;
+            if ($class instanceof Model && is_null($action))
+                return $permission->whereKey($class->getKey());
+            if (!is_string($class) || !is_string($action)) return null;
+            return $permission->action($action)->class($class);
         })->exists();
     }
 }
