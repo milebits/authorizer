969facaea09f5f442353a4fd4e0098a3fe807947
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-18 15:39:37 +0100

    Feature: Adding Permissions install command
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

---
+++ src/Console/Commands/InstallPermissions.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Milebits\Authorizer\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use JetBrains\PhpStorm\ArrayShape;
+use Milebits\Authorizer\Models\Permission;
+use function Milebits\Authorizer\Helpers\app_models;
+
+class InstallPermissions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'authorizer:permInstall';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install Authorizer Permissions';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        Permission::all()->each(fn(Permission $permission) => $permission->forceDelete());
+        app_models()->each(function ($model, $index) {
+            collect($this->getMethods())->each(function ($name, $verb) use ($model) {
+                $modelName = Str::of(class_basename($model))->singular()->snake()->lower();
+                Permission::create(["class" => $model, "action" => $verb, "enabled" => true, 'name' => "$name $modelName"]);
+            });
+        });
+        return 0;
+    }
+
+    /**
+     * @return string[]
+     */
+    #[ArrayShape(['viewAny' => "string", 'view' => "string", "create" => "string", 'update' => "string", 'delete' => "string", 'forceDelete' => "string"])]
+    public function getMethods(): array
+    {
+        return ['viewAny' => "View any", 'view' => "View", "create" => "Create", 'update' => "Update", 'delete' => "Delete", 'forceDelete' => "Force delete"];
+    }
+}
